a25009d806a933eef00fe54101c461a4c8439682
Anton.TkachouITechArt <Anton.Tkachou@itechart-group.com>
2021-10-13 12:49:58 +0300

    We are the champion !!!

--- topic1/task1_2.py
+++ topic1/task1_2.py
@@ -1,39 +1,34 @@
 def analyze_students(data: dict) -> set:
-    """get dict of student -> return set of tuple {(student, object name, multiple marks), ...} """
+    """get dict of student -> return set of tuple {(student, object name,
+    multiple marks), ...} """
     return {tuple(
         [student_name, obj_name, functools.reduce(lambda x, y: x * y, marks)])
-            for student_name, dtstudent in
-            data.items() for obj_name, marks in dtstudent.items() if
-            obj_name != '1C'}
+        for student_name, dtstudent in
+        data.items() for obj_name, marks in dtstudent.items() if
+        obj_name != '1C'}
 
 
 def validate_data(data: dict) -> bool:
-    """Validate data: check input student_name,object_name are str and are only english letters, also check marks are
-    int type """
+    """Validate data: check input student_name,object_name are str and are
+    only english letters, also check marks are int type """
     for student_name, dt_student in data.items():
 
         if not isinstance(student_name, str):
             raise TypeError
 
+        if not student_name.isalnum():
+            raise ValueError
+
         for obj_name, marks in dt_student.items():
             if not isinstance(obj_name, str):
                 raise TypeError
+
+            if not obj_name.isalnum():
+                raise ValueError
 
             for el in marks:
                 if not isinstance(el, int) or isinstance(el, bool):
                     raise TypeError
                 elif not (1 <= el <= 10):
                     raise ValueError
-                else:
-                    pass
-
-            # if not obj_name.isalpha():
-            #     raise ValueError
-            if not isinstance(obj_name, str):
-                raise ValueError
-
-
-        if not student_name.isalpha():
-            raise ValueError
-
     return True

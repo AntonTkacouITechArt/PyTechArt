c02c4c160fe1944ab99fc242b3bee577aac5cadd
Anton.TkachouITechArt <Anton.Tkachou@itechart-group.com>
2021-10-13 10:52:46 +0300

    +why isnt work

--- topic2/task4.py
+++ topic2/task4.py
@@ -1,36 +1,47 @@
-import types
+def pep8_warrior(class_name, bases, attrs):
+    a = {}
+    for name, val in attrs.items():
+        if name.startswith('__'):
+            a[name] = val
+        elif isinstance(val, types.FunctionType):
+            a[name.lower()] = val
+        elif isinstance(val, type):
+            buff = name.split('_')
+            buff = [el.title() for el in buff]
+            buff = ''.join(buff)
+            a[buff] = val
+        else:
+            a[name.upper()] = val
+    return type(class_name, bases, a)
 
 
-def pep8_warrior():
-    pass
-
-
-# how to use types, dont know really
 class PEp8Warrior(type):
     def __new__(self, class_name, bases, attrs):
-        print(attrs)
-
         a = {}
         for name, val in attrs.items():
             if name.startswith('__'):
                 a[name] = val
             elif isinstance(val, types.FunctionType):
                 a[name.lower()] = val
-            elif type(val) == type:
-                a[]
+            elif isinstance(val, type):
+                buff = name.split('_')
+                buff = [el.title() for el in buff]
+                buff = ''.join(buff)
+                a[buff] = val
             else:
                 a[name.upper()] = val
-        return a
+        return type(class_name, bases, a)
 
 
-class D:
-    test = 5
-
-class Dog(metaclass=PEp8Warrior):
-    x = 5
-    y = 8
-
-    def heLLo(self):
-        print('Hi')
-
-print(Dog)
+# class Dog(metaclass=PEp8Warrior):
+#     class test_db:
+#         test = 5
+#
+#     class kek:
+#         test1 = 5
+#
+#     x = 5
+#     y = 8
+#
+#     def heLLo(self):
+#         print('Hi')

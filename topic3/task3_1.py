27a45870a794269cd317d359da8c21eb69832297
Anton.TkachouITechArt <Anton.Tkachou@itechart-group.com>
2021-10-14 13:01:54 +0300

    +add some code to task3_2

--- topic3/task3_1.py
+++ topic3/task3_1.py
@@ -1,32 +1,31 @@
-import re, typing
+import typing, re
 
 
 class RegParser:
-    ADDRESS_REGEX = '([A-Z][a-z]*, )?([A-Z][a-z]*( city| City)?, )?[_\w\s-]+(, | str., )?(\d+\s*[-/\\,|]\s*\d+)'
+    ADDRESS_REGEX = '((?:[A-Z][a-z]*, )?(?:[A-Z][a-z]*(?: city| City)?, )?[_\w\s-]+(?:, | str., )?(?:\d+\s*[-/\\,|]\s*\d+))'
     CONTACT_REGEX = '(age=[\w\s-]+;?)?(name=[\w\s-]+;?)?(surname=[\w\s-]+;?)?(city=[\w\s-]+;?)?'
-    PRICE_REGEX = '(?:(?!^[$€] )(\d+(?:.|,)?\d*))|(?:(\d+(?:.|,)?\d*)(?=\s*BYN$))'
+    PRICE_REGEX = '(?:(?<=^[$€] )(\d+(?:\.|,)?\d*))|(?:(\d+(?:\.|,)?\d*)(?=\s*BYN))'
 
     @classmethod
     def find(cls, text: typing.Optional[str], choice: typing.Optional[int]):
         if choice == 1:
             exp = re.compile(RegParser.ADDRESS_REGEX)
-            print(exp.findall(text))
-            print(exp.match(text).group())
+            return exp.findall(text)
         elif choice == 2:
             pass
         elif choice == 3:
             exp = re.compile(RegParser.PRICE_REGEX)
-            print(exp.match(text))
-            print(exp.match(text).group())
             print(exp.findall(text))
-            print(exp.match(text).group())
+            data = [
+                float(el[0].replace(',', '.')) if '.' in el[0] or ',' in el[
+                    0] else int(el[0]) for el in exp.findall(text)]
+            return data
         else:
             print("Incorrect choice number! Try again!")
-
 
 if __name__ == '__main__':
     text1 = 'Belarus, Minsk city, Tolstoy str., 8 - 0303 Germany, Berlin City, Gauven, 23 - 1203'
     text2 = 'name=Alex;age=20;city=Minsk city;surname=Larkin'
-    text3 = '13.28 BYN $ 5 $ 36 2312,231 BYN'
+    text3 = '$ 5 $ 32,2 232,3232 BYN 2131 $ 232,2'
 
     RegParser.find(text3, 3)
---
+++ topic3/task3_2.py
@@ -0,0 +1,24 @@
+import csv, typing, json
+
+result = {}
+
+
+def merge_students_data(csv_file, xlsx_workbook, json_file):
+    with open(csv_file, newline='') as csv_file:
+        data_from_csv = csv.reader(csv_file, delimiter=' ')
+        print(data_from_csv)
+        for row in data_from_csv:
+            key = ' '.join(row[0:2])
+            result[key] = row[2]
+        print(result)
+    with open(xlsx_workbook, newline='') as xlsx_file:
+        data_from_xlsx = csv.reader(xlsx_file)
+        print(data_from_xlsx)
+        for row in data_from_xlsx:
+            print(row)
+
+    with open(json_file, 'w+', ) as json_write:
+        json.dump(result, json_write, indent=0)
+
+
+merge_students_data('datacsv.csv', 'dataxlsx.xlsx', 'exmaple.json')

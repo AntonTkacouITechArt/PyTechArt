5a7e68631f5f31b97c4dc2236c70253e3b2851d2
NTN-code <tk2007m@mail.ru>
2021-10-14 22:16:53 +0300

    -delete some methods

--- topic4/DB_1.py
+++ topic4/DB_1.py
@@ -1,6 +1,6 @@
 import psycopg2
 from psycopg2 import Error
-
+import typing
 
 class SQLManager:
     def __init__(self, dbname, db_user, password, hostname):
@@ -16,7 +16,6 @@ class SQLManager:
     # CREATE METHOD
 
     def create_table(self):
-
         self.cur.execute("""
                     CREATE TABLE IF NOT EXISTS Shops(
                         id SERIAL PRIMARY KEY,
@@ -46,199 +45,168 @@ class SQLManager:
     # INSERT METHOD
 
     def insert_data(self):
-        self.cur.execute("""
-            INSERT INTO Shops(name, address, staff_amount) 
-            VALUES (%s,%s,%s);
-            """, ('Auchan', None, 250))
-        self.cur.commit()
-        self.cur.execute("""
-            INSERT INTO Shops(name, address, staff_amount) 
-            VALUES (%s,%s,%s);
-            """, ('IKEA', 'Street Žirnių g. 56, Vilnius, Lithuania.', 500))
+        query_insert_into_shops = """
+        INSERT INTO Shops(name, address, staff_amount) 
+        VALUES (%s,%s,%s);
+        """
+        shops_data = (
+            ('Auchan', None, 250),
+            ('IKEA', 'Street Žirnių g. 56, Vilnius, Lithuania.', 500),
+        )
+        self.cur.executemany(query_insert_into_shops, shops_data)
         self.cur.commit()
 
-        self.cur.execute("""
+        query_insert_into_departments = """
                 INSERT INTO Departments(sphere, staff_amount, shop_id) 
                 VALUES (%s,%s,%s);
-            """, ('Furniture', 250, 1))
-        self.cur.commit()
-        self.cur.execute("""
-                INSERT INTO Departments(sphere, staff_amount, shop_id) 
-                VALUES (%s,%s,%s);
-            """, ('Furniture', 300, 2))
-        self.cur.commit()
-        self.cur.execute("""
-                INSERT INTO Departments(sphere, staff_amount, shop_id) 
-                VALUES (%s,%s,%s);
-            """, ('Dishes', 200, 2))
+            """
+        departments_data = (
+            ('Furniture', 250, 1),
+            ('Furniture', 300, 2)
+            ('Dishes', 200, 2)
+        )
+        self.cur.executemany(query_insert_into_departments, departments_data)
         self.cur.commit()
 
-        self.cur.execute("""
+
+        query_insert_into_items = """
                 INSERT INTO Items(name, description, price, department_id) 
                 VALUES (%s,%s,%s,%s);
-            """, ('Table', 'Cheap wooden table', 300, 1))
-        self.cur.commit()
-
-        self.cur.execute("""
-                INSERT INTO Items(name, description, price, department_id) 
-                VALUES (%s,%s,%s,%s);
-            """, ('Table', None, 750, 2))
-        self.cur.commit()
-
-        self.cur.execute("""
-                INSERT INTO Items(name, description, price, department_id) 
-                VALUES (%s,%s,%s,%s);
-            """, ('Bed', 'Amazing wooden bed', 1200, 2))
-        self.cur.commit()
-
-        self.cur.execute("""
-                INSERT INTO Items(name, description, price, department_id) 
-                VALUES (%s,%s,%s,%s);
-            """, ('Cup', None, 10, 3))
-        self.cur.commit()
-
-        self.cur.execute("""
-                INSERT INTO Items(name, description, price, department_id) 
-                VALUES (%s,%s,%s,%s);
-            """, ('Plate', 'Glass plate', 20, 3))
+            """
+        items_data = (
+            ('Table', 'Cheap wooden table', 300, 1),
+            ('Table', None, 750, 2),
+            ('Bed', 'Amazing wooden bed', 1200, 2),
+            ('Cup', None, 10, 3),
+            ('Plate', 'Glass plate', 20, 3),
+        )
+        self.cur.executemany(items_data)
         self.cur.commit()
 
 
-    def select_1(self):
-        self.cur.execute("""
-            SELECT * FROM Items
-            WHERE description IS NOT NULL;
-        """)
-        data = self.cur.fetchall()
-        return data
-
-    def select_2(self):
-        self.cur.execute("""
-                    SELECT DISTINCT sphere FROM Departments
-                    WHERE staff_amount > 200;
-                """)
-        data = self.cur.fetchall()
-        return data
-
-    def select_3(self):
-        self.cur.execute("""
-                    SELECT address FROM Shops
-                    WHERE name ~ '^(I|i)';
-                """)
-        data = self.cur.fetchall()
-        return data
-
-    def select_4(self):
-        self.cur.execute("""
-                    SELECT Items.name FROM Items
-                    INNER JOIN Departments ON Departments.id = Items.department_id
-                    WHERE Departments.sphere = 'Furniture';
-                """)
-        data = self.cur.fetchall()
-        return data
-
-    def select_5(self):
-        self.cur.execute("""
-                    SELECT Shops.name FROM Shops
-                    INNER JOIN Departments ON Departments.shop_id = Shops.id
-                    INNER JOIN Items ON Items.department_id = Departments.id
-                    WHERE Items.description IS NOT NULL;
-                """)
-        data = self.cur.fetchall()
-        return data
-
-    def select_6(self):
-        self.cur.execute("""
-            SELECT 
-                Items.name, description, price,
-                'department_' || sphere AS dep_sphere,
-                'department_' || Departments.staff_amount AS dep_staff,
-                'shop_' || Shops.name AS shop_name,
-                'shop_' || address AS shop_addr, 
-                'shop_' || Shops.staff_amount as shop_staff
-            FROM Items
-            INNER JOIN Departments ON Departments.id = Items.department_id
-            INNER JOIN Shops ON Shops.id = Departments.shop_id;
-        """)
-        data = self.cur.fetchall()
-        return data
-
-    def select_7(self):
-        self.cur.execute("""
-                    SELECT id FROM Items
-                    ORDER BY name
-                    LIMIT 2
-                    OFFSET 3;
-                """)
-        data = self.cur.fetchall()
-        return data
-
-    def select_8(self):
-        self.cur.execute("""
-                            SELECT Items.name, Shops.name FROM Items
-                            INNER JOIN Departments ON Items.department_id = Departments.id
-                            INNER JOIN Shops ON Shops.id = Departments.shop_id;
-                        """)
-        data = self.cur.fetchall()
-        return data
-
-    def select_9(self):
-        self.cur.execute("""
-                            SELECT Items.name, Departments.id FROM Items
-                            LEFT JOIN Departments ON Items.department_id = Departments.id
+    def select_data(self, choice:typing.Optional[int]):
+        if choice == 1:
+            self.cur.execute("""
+                        SELECT * FROM Items
+                        WHERE description IS NOT NULL;
                     """)
-        data = self.cur.fetchall()
-        return data
-
-    def select_10(self):
-        self.cur.execute("""
-                            SELECT Items.name, Departments.id FROM Items
-                            RIGHT JOIN Departments ON Items.department_id = Departments.id
-                        """)
-        data = self.cur.fetchall()
-        return data
-
-    def select_11(self):
-        self.cur.execute("""
-                            SELECT Items.name, Departments.id FROM Items
-                            FULL JOIN Departments ON Items.department_id = Departments.id
+            data = self.cur.fetchall()
+            return data
+        elif choice == 2:
+            self.cur.execute("""
+                                SELECT DISTINCT sphere FROM Departments
+                                WHERE staff_amount > 200;
+                            """)
+            data = self.cur.fetchall()
+            return data
+        elif choice == 3:
+            self.cur.execute("""
+                        SELECT address FROM Shops
+                        WHERE name ~ '^(I|i)';
                     """)
-        data = self.cur.fetchall()
-        return data
-
-    def select_12(self):
-        self.cur.execute("""
-                            SELECT Items.name, Shops.name FROM Items
-                            CROSS JOIN Departments
-                            INNER JOIN Shops ON Shops.id = Departments.shop_id;
-                    """)
-        data = self.cur.fetchall()
-        return data
-
-    def select_13(self):
-        self.cur.execute("""
-                SELECT Shops.name, COUNT(*) as count_goods, SUM(price),
-                MAX(price), MIN(price),
-                AVG(price) FROM Items
-                INNER JOIN Departments ON Departments.id = Items.department_id
-                INNER JOIN Shops ON Shops.id = Departments.shop_id
-                GROUP BY (Shops.name);
-                    """)
-        data = self.cur.fetchall()
-        return data
-
-    def select_14(self):
-        self.cur.execute("""
-                        SELECT Shops.name, Items.name FROM Items
+            data = self.cur.fetchall()
+            return data
+        elif choice == 4:
+            self.cur.execute("""
+                        SELECT Items.name FROM Items
                         INNER JOIN Departments ON Departments.id = Items.department_id
-                        INNER JOIN Shops ON Shops.id = Departments.shop_id;
+                        WHERE Departments.sphere = 'Furniture';
                     """)
-        data = self.cur.fetchall()
-        new_data = {}
-        for el in data:
-            new_data.setdefault(el[0], []).append(el[1])
-        print(new_data)
-        return new_data
+            data = self.cur.fetchall()
+            return data
+        elif choice == 5:
+            self.cur.execute("""
+                        SELECT Shops.name FROM Shops
+                        INNER JOIN Departments ON Departments.shop_id = Shops.id
+                        INNER JOIN Items ON Items.department_id = Departments.id
+                        WHERE Items.description IS NOT NULL;
+                    """)
+            data = self.cur.fetchall()
+            return data
+        elif choice == 6:
+            self.cur.execute("""
+                SELECT 
+                    Items.name, description, price,
+                    'department_' || sphere AS dep_sphere,
+                    'department_' || Departments.staff_amount AS dep_staff,
+                    'shop_' || Shops.name AS shop_name,
+                    'shop_' || address AS shop_addr, 
+                    'shop_' || Shops.staff_amount as shop_staff
+                FROM Items
+                INNER JOIN Departments ON Departments.id = Items.department_id
+                INNER JOIN Shops ON Shops.id = Departments.shop_id;
+            """)
+            data = self.cur.fetchall()
+            return data
+        elif choice == 7:
+            self.cur.execute("""
+                        SELECT id FROM Items
+                        ORDER BY name
+                        LIMIT 2
+                        OFFSET 3;
+                    """)
+            data = self.cur.fetchall()
+            return data
+        elif choice == 8:
+            self.cur.execute("""
+                                SELECT Items.name, Shops.name FROM Items
+                                INNER JOIN Departments ON Items.department_id = Departments.id
+                                INNER JOIN Shops ON Shops.id = Departments.shop_id;
+                            """)
+            data = self.cur.fetchall()
+            return data
+        elif choice == 9:
+            self.cur.execute("""
+                                SELECT Items.name, Departments.id FROM Items
+                                LEFT JOIN Departments ON Items.department_id = Departments.id
+                        """)
+            data = self.cur.fetchall()
+            return data
+        elif choice == 10:
+            self.cur.execute("""
+                                SELECT Items.name, Departments.id FROM Items
+                                RIGHT JOIN Departments ON Items.department_id = Departments.id
+                            """)
+            data = self.cur.fetchall()
+            return data
+        elif choice == 11:
+            self.cur.execute("""
+                                SELECT Items.name, Departments.id FROM Items
+                                FULL JOIN Departments ON Items.department_id = Departments.id
+                        """)
+            data = self.cur.fetchall()
+            return data
+        elif choice == 12:
+            self.cur.execute("""
+                                SELECT Items.name, Shops.name FROM Items
+                                CROSS JOIN Departments
+                                INNER JOIN Shops ON Shops.id = Departments.shop_id;
+                        """)
+            data = self.cur.fetchall()
+            return data
+        elif choice == 13:
+            self.cur.execute("""
+                    SELECT Shops.name, COUNT(*) as count_goods, SUM(price),
+                    MAX(price), MIN(price),
+                    AVG(price) FROM Items
+                    INNER JOIN Departments ON Departments.id = Items.department_id
+                    INNER JOIN Shops ON Shops.id = Departments.shop_id
+                    GROUP BY (Shops.name);
+                        """)
+            data = self.cur.fetchall()
+            return data
+        elif choice == 14:
+            self.cur.execute("""
+                            SELECT Shops.name, Items.name FROM Items
+                            INNER JOIN Departments ON Departments.id = Items.department_id
+                            INNER JOIN Shops ON Shops.id = Departments.shop_id;
+                        """)
+            data = self.cur.fetchall()
+            new_data = {}
+            for el in data:
+                new_data.setdefault(el[0], []).append(el[1])
+            return new_data
+
 
     # UPDATE METHOD
 
@@ -251,14 +219,13 @@ class SQLManager:
 
     # DELETE METHODS
 
-    def delete_1(self):
+    def delete_data(self):
         self.cur.execute("""
-            DELETE FROM Items
-            WHERE price > 500 AND description IS NULL;
-        """)
+                    DELETE FROM Items
+                    WHERE price > 500 AND description IS NULL;
+                """)
         self.cur.commit()
 
-    def delete_2(self):
         self.cur.execute("""
             DELETE FROM Items WHERE id IN 
             (
@@ -270,7 +237,6 @@ class SQLManager:
         """)
         self.cur.commit()
 
-    def delete_3(self):
         self.cur.execute("""
             DELETE FROM Items
             WHERE Items.id IN (
@@ -281,7 +247,6 @@ class SQLManager:
         """)
         self.cur.commit()
 
-    def delete_4(self):
         self.cur.execute("""
             TRUNCATE TABLE Shops CASCADE;
             TRUNCATE TABLE Departments CASCADE;

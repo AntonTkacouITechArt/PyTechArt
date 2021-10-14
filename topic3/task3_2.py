4b6c2fd7428218bc8a60e5fd55492cca6fbf430c
Anton.TkachouITechArt <Anton.Tkachou@itechart-group.com>
2021-10-14 13:42:11 +0300

    +in task3_2 delete empthy str

--- topic3/task3_2.py
+++ topic3/task3_2.py
@@ -9,13 +9,14 @@ def merge_students_data(csv_file, xlsx_workbook, json_file):
         print(data_from_csv)
         for row in data_from_csv:
             key = ' '.join(row[0:2])
-            result[key] = row[2]
+            result[key] = {row[2]: []}
         print(result)
     with open(xlsx_workbook, newline='') as xlsx_file:
         data_from_xlsx = csv.reader(xlsx_file)
         print(data_from_xlsx)
         for row in data_from_xlsx:
-            print(row)
+          result[row[0]]=row[1:-1]
+    print(result)
 
     with open(json_file, 'w+', ) as json_write:
         json.dump(result, json_write, indent=0)

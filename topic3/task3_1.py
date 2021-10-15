d152aac3319ab21cd4f4297eec833b776d5f0cdf
Anton.TkachouITechArt <Anton.Tkachou@itechart-group.com>
2021-10-15 11:01:49 +0300

    +fixing regex +add some code to task3_2

--- topic3/task3_1.py
+++ topic3/task3_1.py
@@ -3,22 +3,29 @@ import typing, re
 
 class RegParser:
     ADDRESS_REGEX = '((?:[A-Z][a-z]*, )?(?:[A-Z][a-z]*(?: city| City)?, )?[_\w\s-]+(?:, | str., )?(?:\d+\s*[-/\\,|]\s*\d+))'
-    CONTACT_REGEX = '(age=[\w\s-]+;?)?(name=[\w\s-]+;?)?(surname=[\w\s-]+;?)?(city=[\w\s-]+;?)?'
-    PRICE_REGEX = '(?:(?<=^[$€] )(\d+(?:\.|,)?\d*))|(?:(\d+(?:\.|,)?\d*)(?=\s*BYN))'
+    # CONTACT_REGEX = '(age=[\w\s-]+;?)?(name=[\w\s-]+;?)?(surname=[\w\s-]+;?)?(city=[\w\s-]+;?)?'
+    CONTACT_REGEX = '(name=[\w\s-]+;?)?(age=[\w\s-]+;?)?(city=[\w\s-]+;?)?(surname=[\w\s-]+;?)?'
+    PRICE_REGEX = '(?:(?<=[$€] )(\d+(?:\.|,)?\d*))|(?:(\d+(?:\.|,)?\d*)(?=\s*BYN))'
+
 
     @classmethod
     def find(cls, text: typing.Optional[str], choice: typing.Optional[int]):
+        data = None
         if choice == 1:
             exp = re.compile(RegParser.ADDRESS_REGEX)
             return exp.findall(text)
         elif choice == 2:
-            pass
+            # fixing empty string(None) and truble with regex
+            exp = re.compile(RegParser.CONTACT_REGEX)
+            print(exp.findall(text))
+            a = exp.findall(text)
+
+            return data
         elif choice == 3:
+            # fixing empty string(None)
             exp = re.compile(RegParser.PRICE_REGEX)
             print(exp.findall(text))
-            data = [
-                float(el[0].replace(',', '.')) if '.' in el[0] or ',' in el[
-                    0] else int(el[0]) for el in exp.findall(text)]
+            print(exp.group())
             return data
         else:
             print("Incorrect choice number! Try again!")
@@ -27,5 +34,6 @@ if __name__ == '__main__':
     text1 = 'Belarus, Minsk city, Tolstoy str., 8 - 0303 Germany, Berlin City, Gauven, 23 - 1203'
     text2 = 'name=Alex;age=20;city=Minsk city;surname=Larkin'
     text3 = '$ 5 $ 32,2 232,3232 BYN 2131 $ 232,2'
+    text4 = 'age=20;city=Minsk city;surname=Larkin;name=Alex'
 
-    RegParser.find(text3, 3)
+    RegParser.find(text4, 2)
--- topic3/task3_2.py
+++ topic3/task3_2.py
@@ -1,25 +1,20 @@
-import csv, typing, json
-
 result = {}
 
+#i dont know why
+def merge_students_data(csv_file: typing.Optional[file],
+                        xlsx_workbook: typing.Optional[file],
+                        json_file: typing.Optional[file]) -> typing.Dict[
+                        str, typing.Dict[int, typing.List[int]]]:
+    data_from_csv = csv.reader(csv_file)
+    for row in data_from_csv:
+        key = ' '.join(row[0:2])
+        result[key] = row[2]
 
-def merge_students_data(csv_file, xlsx_workbook, json_file):
-    with open(csv_file, newline='') as csv_file:
-        data_from_csv = csv.reader(csv_file, delimiter=' ')
-        print(data_from_csv)
-        for row in data_from_csv:
-            key = ' '.join(row[0:2])
-            result[key] = {row[2]: []}
-        print(result)
-    with open(xlsx_workbook, newline='') as xlsx_file:
-        data_from_xlsx = csv.reader(xlsx_file)
-        print(data_from_xlsx)
-        for row in data_from_xlsx:
-          result[row[0]]=row[1:-1]
-    print(result)
+    data_from_xlsx = csv.reader(xlsx_workbook, delimiter=' ',)
+    for i, row in enumerate(data_from_xlsx):
+        row = list(filter(None, row))
+        result[row[0]] = {result[row[0]]: row[1:-1]}
 
-    with open(json_file, 'w+', ) as json_write:
-        json.dump(result, json_write, indent=0)
+    json.dump(result, json_file, indent=0)
 
-
-merge_students_data('datacsv.csv', 'dataxlsx.xlsx', 'exmaple.json')
+# merge_students_data('datacsv.csv', 'dataxlsx.xlsx', 'exmaple.json')

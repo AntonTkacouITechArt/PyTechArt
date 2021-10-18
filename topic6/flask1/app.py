ea341607a09382c5d3cf25a1084351cce4c9869d
Anton.TkachouITechArt <Anton.Tkachou@itechart-group.com>
2021-10-18 12:45:11 +0300

    +start flask

---
+++ topic6/flask1/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+@app.route('/<name>/', methods=['GET', ])
+def use_escape_for_unsecrity_data(name):
+    return f'<h1>Hello, {escape(name)}</h1>'
+
+@app.route('/index/')
+@app.route('/index/<string:name>',methods=['GET', ])
+def index_page(name=None):
+    return render_template('index.html', name=name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

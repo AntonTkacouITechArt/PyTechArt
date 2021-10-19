91ba427b35ad21d4f5b714ec77861a070c7c76e3
Anton.TkachouITechArt <Anton.Tkachou@itechart-group.com>
2021-10-19 12:17:29 +0300

    +refactor +add functionality

--- topic5/task1.py
+++ topic5/task1.py
@@ -4,35 +4,30 @@ import json
 
 def func_1(name_currency:typing.Optional[str]) -> typing.Optional[float]:
     """Get three-digit alphabetic currency(str) code and return BYN currency rate(float)"""
+    response = None
     url = f'https://www.nbrb.by/api/exrates/rates/{name_currency}'
     params = {
         'parammode':2,
         'periodicity':0,
     }
-    response = None
     try:
         response = requests.get(url=url, params=params)
-        print(response.status_code)
-        # If the response was successful, no Exception will be raised
+        print('status:' + str(response.status_code))
         response.raise_for_status()
-    except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')  # Python 3.6
-    except Exception as err:
-        print(f'Other error occurred: {err}')  # Python 3.6
-
-    # print(data['Cur_OfficialRate'])
-    # print(data)
-    data = response.json()
-    if data['Cur_OfficialRate'] is not None:
-        return round(data['Cur_OfficialRate'], 2)
-    else:
+        data = response.json()
+        if data['Cur_OfficialRate'] is not None:
+            return round(float(data['Cur_Scale'])/float(data['Cur_OfficialRate']), 2)
         raise
+    except requests.HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+
 
 def func_2():
     pass
 
 
 if __name__ == '__main__':
-    # print(func_1('USD'))
-    # print(func_1('RUB'))
-    func_2()
+    print(func_1('USD'))
+    print(func_1('RUB'))

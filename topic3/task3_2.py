fb53c009532525938daf812ca2ab606c9c58cbee
Anton.TkachouITechArt <Anton.Tkachou@itechart-group.com>
2021-10-20 11:10:06 +0300

    +edit task3_2

--- topic3/task3_2.py
+++ topic3/task3_2.py
@@ -3,14 +3,23 @@ result = {}
 
 def merge_students_data(csv_file: typing.Optional[typing.IO],
                         xlsx_workbook: typing.Optional[typing.IO],
-                        json_file: typing.Optional[typing.IO],):
+                        json_file: typing.Optional[typing.IO], ):
     data_from_csv = csv.reader(csv_file, delimiter=',')
     next(data_from_csv, None)
     result = {' '.join(row[0:2]): int(row[2]) for row in data_from_csv}
-    data_from_xlsx = csv.reader(xlsx_workbook, delimiter=',',)
-    for row in data_from_xlsx:
-        print(row)
-        name = ' '.join(row[0:1])
-        nums = [int(num) for num in list(filter(None, row[1:-1]))]
-        result[name] = {result[name]: nums}
+    # replace List1 =>  Pi to debug
+    list1 = xlsx_workbook['List1']
+    for i, row in enumerate(list1.rows):
+        if i != 0:
+            age = result[row[0].value]
+            result[row[0].value] = {result[row[0].value]: []}
+            for col in row[1:-1]:
+                if col.value is not None:
+                    result[row[0].value][age].append(col.value)
     json.dump(result, json_file, indent=0)
+
+# if __name__ == '__main__':
+#     xlsx_workbook = load_workbook('empty_book.xlsx', )
+#     csv_file = open('datacsv.csv', 'r')
+#     json_file = open('exmaple.json', 'w')
+#     merge_students_data(csv_file, xlsx_workbook, json_file)

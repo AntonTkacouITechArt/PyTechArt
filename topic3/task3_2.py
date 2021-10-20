6a27eeb229d298c6ee460a07038033bfca43df1c
NTN-code <tk2007m@mail.ru>
2021-10-20 14:55:32 +0300

    +?

--- topic3/task3_2.py
+++ topic3/task3_2.py
@@ -1,3 +1,5 @@
+import typing, csv, json
+from openpyxl import load_workbook
 result = {}
 
 
@@ -8,7 +10,7 @@ def merge_students_data(csv_file: typing.Optional[typing.IO],
     next(data_from_csv, None)
     result = {' '.join(row[0:2]): int(row[2]) for row in data_from_csv}
     # replace List1 =>  Pi to debug
-    list1 = xlsx_workbook['List1']
+    list1 = xlsx_workbook['Pi']
     # list1 = xlsx_workbook['List1']
     for i, row in enumerate(list1.rows):
         if i != 0:
@@ -18,7 +20,7 @@ def merge_students_data(csv_file: typing.Optional[typing.IO],
                     result[row[0].value]['marks'].append(col.value)
     json.dump(result, json_file, indent=0)
 
- if __name__ == '__main__':
+if __name__ == '__main__':
      xlsx_workbook = load_workbook('empty_book.xlsx', )
      csv_file = open('datacsv.csv', 'r')
      json_file = open('exmaple.json', 'w')

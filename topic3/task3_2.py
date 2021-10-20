8d54359c54a4a15c21d456cb20fd13a0bef57460
NTN-code <tk2007m@mail.ru>
2021-10-20 19:28:24 +0300

    +clean code

--- topic3/task3_2.py
+++ topic3/task3_2.py
@@ -7,8 +7,6 @@ def merge_students_data(csv_file: typing.Optional[typing.IO],
     data_from_csv = csv.reader(csv_file, delimiter=',')
     next(data_from_csv, None)
     result = {' '.join(row[0:2]): int(row[2]) for row in data_from_csv}
-    # replace List1 =>  Pi to debug
-    # list1 = xlsx_workbook['Pi']
     list1 = xlsx_workbook['List1']
     for i, row in enumerate(list1.rows):
         result[row[0].value] = {'age': result[row[0].value], 'marks': []}

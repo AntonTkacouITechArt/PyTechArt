192f6c629583a0e953e97f591a15ccac863baac2
NTN-code <tk2007m@mail.ru>
2021-10-20 14:53:04 +0300

    +reformat result

--- topic3/task3_2.py
+++ topic3/task3_2.py
@@ -9,17 +9,17 @@ def merge_students_data(csv_file: typing.Optional[typing.IO],
     result = {' '.join(row[0:2]): int(row[2]) for row in data_from_csv}
     # replace List1 =>  Pi to debug
     list1 = xlsx_workbook['List1']
+    # list1 = xlsx_workbook['List1']
     for i, row in enumerate(list1.rows):
         if i != 0:
-            age = result[row[0].value]
-            result[row[0].value] = {result[row[0].value]: []}
+            result[row[0].value] = {'age': result[row[0].value], 'marks': []}
             for col in row[1:-1]:
                 if col.value is not None:
-                    result[row[0].value][age].append(col.value)
+                    result[row[0].value]['marks'].append(col.value)
     json.dump(result, json_file, indent=0)
 
-# if __name__ == '__main__':
-#     xlsx_workbook = load_workbook('empty_book.xlsx', )
-#     csv_file = open('datacsv.csv', 'r')
-#     json_file = open('exmaple.json', 'w')
-#     merge_students_data(csv_file, xlsx_workbook, json_file)
+ if __name__ == '__main__':
+     xlsx_workbook = load_workbook('empty_book.xlsx', )
+     csv_file = open('datacsv.csv', 'r')
+     json_file = open('exmaple.json', 'w')
+     merge_students_data(csv_file, xlsx_workbook, json_file)

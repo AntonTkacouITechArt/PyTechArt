08c5923ba1164cc0e4e69b9fbfde0cfbdfe28d5f
NTN-code <tk2007m@mail.ru>
2021-10-20 18:53:40 +0300

    +done regex +edit task2

--- topic3/task3_1.py
+++ topic3/task3_1.py
@@ -1,9 +1,7 @@
-import re, typing
 class RegParser:
-    ADDRESS_REGEX = r'(?:^(?:[A-Z][a-z]*, )?(?:[A-Z][a-z]*(?: [Cc]ity)?, )?)[_\w\s-]+(?:, | str., )?(?:\d+\s*[-\/\\,|]\s*\d+)$'
-    # CONTACT_REGEX = r'^(?:(?P<age>age=[\d]+;?)|(?P<name>name=[\w\s-]+;?)|(?P<surname>surname=[\w\s-]+;?)|(?P<city>city=[\w\s-]+;?)){1,4}$'
-    CONTACT_REGEX = r'^(?:(?:(?P<age>age=[\d]+)(?:;)?)|(?:(?:(?P<name>name=[\w\s-]+)(?:;)?))|(?:(?P<surname>surname=[\w\s-]+)(?:;)?)|(?:(?P<city>city=[\w\s-]+)(?:;)?)){1,4}$'
-    PRICE_REGEX = r'(?:(?<=[$€] )(?:\d+(?:\.|,\d+)?\d*))|(?:(?:\d+(?:\.|,\d+)?\d*)(?=[ ]*BYN))'
+    ADDRESS_REGEX = r"""(?:^(?:[A-Z][a-z]*, )?(?:[A-Z][a-z]*(?: [Cc]ity)?, )?)[_\w\s-]+(?:, | str., )?(?:\d+\s*[-\/\\,|]\s*\d+)$"""
+    CONTACT_REGEX = r"""^(?:(?:(?:age=(?P<age>[\d]+))(?:;)?)|(?:(?:(?:name=(?P<name>[\w\s-]+))(?:;)?))|(?:(?:surname=(?P<surname>[\w\s-]+))(?:;)?)|(?:(?:city=(?P<city>[\w\s-]+))(?:;)?)){1,4}$"""
+    PRICE_REGEX = r"""(?:(?<=[$€] )(?:\d+(?:\.|,\d+)?\d*))|(?:(?:\d+(?:\.|,\d+)?\d*)(?=[ ]*BYN))"""
 
     @classmethod
     def find(cls, text: typing.Optional[str], choice: typing.Optional[int]):
@@ -16,47 +14,27 @@ class RegParser:
             data = []
             for person in newdata:
                 dict_per = {}
-                for info in person:
+                for i, info in enumerate(person):
                     if info != '':
-                        el = info.replace(';', '').split('=')
-                        dict_per.update({el[0]: el[1]})
+                        if i == 0:
+                            dict_per.update({'age': info})
+                        elif i == 1:
+                            dict_per.update({'name': info})
+                        elif i == 2:
+                            dict_per.update({'surname': info})
+                        elif i == 3:
+                            dict_per.update({'city': info})
                 data.append(dict_per)
         elif choice == 3:
             groups_combinations = re.findall(RegParser.PRICE_REGEX, text,
                                              re.MULTILINE)
-            print(groups_combinations)
-            # x = [num.replace(',', '.') for group in groups_combinations
-            #      for num in group if num != '']
-            # print(x)
-            # data = [
-            #     int(num) if '.' not in num else float(num.replace(',', '.'))
-            #     for num in x]
             data = [int(num) if ',' not in num and '.' not in num
-                    else float(num.replace(',','.'))
-                    for num in groups_combinations ]
+                    else float(num.replace(',', '.'))
+                    for num in groups_combinations]
         return data
 
 if __name__ == '__main__':
-    text1 = """Russia, Moscow City, Tolstoy- 123_456 str., 123 | 16
-russia, Moscow City, Tolstoy str., 123 | 16
-Russia, moscow City, Tolstoy str., 123 | 16
-Russia, Moscow City, Tolstoy str., 123 | 16
-Russia, Moscow City, Tolstoystr., 123 | 16
-Russia, Moscow, Tolstoy, 123 , 16
-Moscow, Tolstoy, 123-16
-Moscow, Tolstoy, 123\16
- Moscow, Tolstoy, 123\16
-Moscow, Tolstoy, 123\16"""
-    text2 = """age=20;name=Alex;city=Minsk city
-age=20;name=Alex;city=Minsk city;surname=Smith;age=22
-age=22
-
-surname=Smith;name=Alex;city=Minsk-city;age=20
-surname=Smith,name=Alex,city=Minsk-city,age=20
- surname=Smith;name=Alex;city=Minsk-city;age=20"""
-    text3="""$ 123, price = 12.123 BYN or € 6,03 and 3.14.
-1321, $ 213. 21312 $ das 312,32
-BYN 4242,32 $ 23.2 322, BYN 323,32 BYN
-23. BYN 32, BYN   3232,23     BYN 
-$  342,323 """
-    print(RegParser.find(text4, 2))
+     xlsx_workbook = load_workbook('empty_book.xlsx', )
+     csv_file = open('datacsv.csv', 'r')
+     json_file = open('exmaple.json', 'w')
+     merge_students_data(csv_file, xlsx_workbook, json_file)

a8843c3fc4ed64539f1a9ba0c9454e157acd01eb
Anton.TkachouITechArt <Anton.Tkachou@itechart-group.com>
2021-10-21 09:55:17 +0300

    fixing task3_2

--- topic3/task3_2.py
+++ topic3/task3_2.py
@@ -6,10 +6,10 @@ def merge_students_data(csv_file: typing.Optional[typing.IO],
                         json_file: typing.Optional[typing.IO], ):
     data_from_csv = csv.reader(csv_file, delimiter=',')
     next(data_from_csv, None)
-    result = {' '.join(row[0:2]): int(row[2]) for row in data_from_csv}
+    result = {' '.join(row[0:2]): {'age': int(row[2])} for row in data_from_csv}
     list1 = xlsx_workbook['List1']
-    for i, row in enumerate(list1.rows):
-        result[row[0].value] = {'age': result[row[0].value], 'marks': []}
+    for row in list1.rows:
+        result[row[0].value]['marks'] = []
         for col in row[1:]:
             if col.value is not None:
                 result[row[0].value]['marks'].append(col.value)

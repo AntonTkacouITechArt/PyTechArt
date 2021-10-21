8a043ac4ea1f36b93ef94229106df292deace4b0
Anton.TkachouITechArt <Anton.Tkachou@itechart-group.com>
2021-10-21 10:25:21 +0300

    +optimize code )

--- topic3/task3_1.py
+++ topic3/task3_1.py
@@ -6,35 +6,23 @@ class RegParser:
     @classmethod
     def find(cls, text: typing.Optional[str], choice: typing.Optional[int]):
         """Find address, contact, price"""
-        data = None
-        if choice == 1:
-            data = re.findall(RegParser.ADDRESS_REGEX, text, re.MULTILINE)
-        elif choice == 2:
-            newdata = re.findall(RegParser.CONTACT_REGEX, text, re.MULTILINE)
-            data = []
-            for person in newdata:
-                dict_per = {}
-                for i, info in enumerate(person):
-                    if info != '':
-                        if i == 0:
-                            dict_per.update({'age': info})
-                        elif i == 1:
-                            dict_per.update({'name': info})
-                        elif i == 2:
-                            dict_per.update({'surname': info})
-                        elif i == 3:
-                            dict_per.update({'city': info})
-                data.append(dict_per)
-        elif choice == 3:
-            groups_combinations = re.findall(RegParser.PRICE_REGEX, text,
+        regex = [
+            lambda text: re.findall(RegParser.ADDRESS_REGEX, text,
+                                    re.MULTILINE),
+            lambda text: re.findall(RegParser.CONTACT_REGEX, text,
+                                    re.MULTILINE),
+            lambda text: re.findall(RegParser.PRICE_REGEX, text,
                                              re.MULTILINE)
+        ]
+        data = None
+        newdata = regex[choice-1](text)
+        if choice == 1:
+            data = newdata
+        elif choice == 2:
+            data = [match.groupdict() for match in
+                      re.finditer(RegParser.CONTACT_REGEX, text, re.MULTILINE)]
+        elif choice == 3:
             data = [int(num) if ',' not in num and '.' not in num
                     else float(num.replace(',', '.'))
-                    for num in groups_combinations]
+                    for num in newdata]
         return data
-
-if __name__ == '__main__':
-     xlsx_workbook = load_workbook('empty_book.xlsx', )
-     csv_file = open('datacsv.csv', 'r')
-     json_file = open('exmaple.json', 'w')
-     merge_students_data(csv_file, xlsx_workbook, json_file)
--- topic3/task3_2.py
+++ topic3/task3_2.py
@@ -6,7 +6,8 @@ def merge_students_data(csv_file: typing.Optional[typing.IO],
                         json_file: typing.Optional[typing.IO], ):
     data_from_csv = csv.reader(csv_file, delimiter=',')
     next(data_from_csv, None)
-    result = {' '.join(row[0:2]): {'age': int(row[2])} for row in data_from_csv}
+    result = {' '.join(row[0:2]): {'age': int(row[2])} for row in
+              data_from_csv}
     list1 = xlsx_workbook['List1']
     for row in list1.rows:
         result[row[0].value]['marks'] = []
@@ -14,3 +15,10 @@ def merge_students_data(csv_file: typing.Optional[typing.IO],
             if col.value is not None:
                 result[row[0].value]['marks'].append(col.value)
     json.dump(result, json_file, indent=0)
+
+
+if __name__ == '__main__':
+    xlsx_workbook = load_workbook('empty_book.xlsx', )
+    csv_file = open('datacsv.csv', 'r')
+    json_file = open('exmaple.json', 'w')
+    merge_students_data(csv_file, xlsx_workbook, json_file)

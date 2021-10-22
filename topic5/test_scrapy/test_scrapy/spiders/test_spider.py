17aed59ec74394126b440d3ef1c4ee3df56785fb
NTN-code <tk2007m@mail.ru>
2021-10-22 16:53:41 +0300

    +push

--- topic5/test_scrapy/test_scrapy/spiders/test_spider.py
+++ topic5/test_scrapy/test_scrapy/spiders/test_spider.py
@@ -6,22 +6,6 @@ class TestSpiderSpider(scrapy.Spider):
     allowed_domains = ['yandex.by']
     start_urls = ['https://yandex.by/pogoda/region/149',]
 
-    # def start_requests(self):
-
-
-    def parse_city_url(self, response, **kwargs):
-        urls = response.css('.place-list__item-name::attr(href)').extract()
-        names = response.css('.place-list__item-name::text').extract()
-        rows_data = list(zip(names, urls))
-        data = []
-        for row in rows_data:
-            if row[0] != 'Беларусь':
-                url = 'https://yandex.by/pogoda'+ row[1]
-                city = row[0]
-                data.append({'url':url,'city':city})
-        yield data
-
-
     def parse(self, response):
         names = response.css('.place-list__item-name::text').extract()
         links = response.css('.place-list__item-name::attr(href)').extract()

868c1278e811309302a405238341cc337a82ccf7
Anton.TkachouITechArt <Anton.Tkachou@itechart-group.com>
2021-10-25 12:18:52 +0300

    +edit app

--- topic6/flask1/app.py
+++ topic6/flask1/app.py
@@ -1,21 +1,29 @@
 from flask import Flask, render_template
 from markupsafe import escape
+from flask import request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'POST':
+        pass
+    return render_template('index.html')
 
-@app.route('/<name>/', methods=['GET', ])
-def use_escape_for_unsecrity_data(name):
-    return f'<h1>Hello, {escape(name)}</h1>'
 
-@app.route('/index/')
-@app.route('/index/<string:name>',methods=['GET', ])
-def index_page(name=None):
-    return render_template('index.html', name=name)
+# @app.route('/')
+# def hello_world():  # put application's code here
+#     return 'Hello World!'
+#
+# @app.route('/<name>/', methods=['GET', ])
+# def use_escape_for_unsecrity_data(name):
+#     return f'<h1>Hello, {escape(name)}</h1>'
+#
+# @app.route('/index/')
+# @app.route('/index/<string:name>',methods=['GET', ])
+# def index_page(name=None):
+#     return render_template('index.html', name=name)
 
 
 if __name__ == '__main__':

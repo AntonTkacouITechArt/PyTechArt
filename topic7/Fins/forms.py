8859f3da2204e61cac1b3d9868795da20f3e0328
NTN-code <tk2007m@mail.ru>
2021-11-04 00:14:09 +0300

    +form......

--- topic7/Fins/forms.py
+++ topic7/Fins/forms.py
@@ -1,25 +1,8 @@
 from django import forms
-from django.forms import Form, BooleanField, ChoiceField, ModelChoiceField
-
-from Fins.models import Shop, Department
-
-FAVORITE_COLORS_CHOICES = [
-    ('blue', 'Blue'),
-    ('green', 'Green'),
-    ('black', 'Black'),
-]
+from django.forms import Form, BooleanField, ChoiceField
 
 
 class CompareForm(Form):
-    # def __init__(self, shop_pk, *args, **kwargs):
-    #     super(CompareForm, self).__init__(*args, **kwargs)
-    #     res = Department.objects.filter(
-    #         shop__exact=kwargs.get('shop_pk')).all()
-    #     print(res)
-    #     self.fields['department_1'].__choices = res
-    #     print(self.fields['department_1'].__dict__)
-    #     print(shop_pk)
-
     department_1 = ChoiceField(
         required=True,
         widget=forms.Select, )
--- topic7/Fins/views/forms_view.py
+++ topic7/Fins/views/forms_view.py
@@ -1,38 +1,46 @@
 from django.shortcuts import render
 from django.views.generic import FormView
-
 from Fins.models import Department
 from Fins.forms import CompareForm
 
 
 class CompareFormView(FormView):
+    form_class = CompareForm
+    template_name = 'forms/forms.html'
+
     def get(self, request, *args, **kwargs):
-#         print(kwargs.get('shop_pk'))
-        department = Department.objects.filter(
-            shop__exact=kwargs.get('shop_pk')).all()
-        data = [dep.id for dep in department]
-        print(data)
-        print(department)
-        form = CompareForm()
-        form['department_1'].__choices = department
+        form = self.get_form(kwargs.get('shop_pk'))
         return render(request, 'forms/forms.html', {'form': form})
-#
-    # def post(self, request, *args, **kwargs):
-    #     department1 = Department.objects.get(
-    #         id=kwargs.get('select_department_1')
-    #     )
-    #     department2 = Department.objects.get(
-    #         id=kwargs.get('select_department_2')
-    #     )
+
+    def get_form(self, shop_pk, *args, **kwargs):
+        department = Department.objects.filter(
+                shop__exact=shop_pk).all()
+        data = [(dep.id, dep.sphere) for dep in department]
+        form = super().get_form(*args, **kwargs)
+        form.fields['department_1'].choices = data
+        form.fields['department_2'].choices = data
+        return form
+
+    # def get(self, request, *args, **kwargs):
+    #     initial = self.get_initial(kwargs.get('shop_pk'))
+    #     form = CompareForm(initial=initial)
+    #     return render(request, 'forms/forms.html', {'form': form})
+
     #
-    #     context = {}
-    #     if request.POST.get('staff_amount') == 'on':
-    #         context.update(
-    #             {
-    #                 'staff_amount1': department1.staff_amount,
-    #                 'staff_amount2': department2.staff_amount,
-    #             }
-    #         )
-    #     if request.POST.get('total_sold_goods') == 'on':
-    #         pass
-    #     # context =
+    # def get_initial(self, shop_id):
+    #     initial = super(CompareFormView, self).get_initial()
+    #     department = Department.objects.filter(
+    #         shop__exact=shop_id).all()
+    #     data = [dep.sphere for dep in department]
+    # data = [(dep.id, dep.sphere) for dep in department]
+    #
+    # initial['department_1'] = Department.models.query(
+    #     ~Q(department_shop=1))
+    # initial['department_2'] = Department.models.query(
+    #     ~Q(department_shop=2))
+    # print(data)
+    # initial['department_1'] = data
+    # initial['department_2'] = data
+    # initial['total_cost_goods'] = True
+    # return initial
+#

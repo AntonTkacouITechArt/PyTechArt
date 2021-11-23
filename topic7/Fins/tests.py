c3b1465f0673326689fd22d7efcb12a614f0eaa2
NTN-code <tk2007m@mail.ru>
2021-11-23 21:32:30 +0300

    +test django, status + selenium

--- topic7/Fins/tests.py
+++ topic7/Fins/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.chrome.webdriver import WebDriver
+
 
 class TestStatusCodePages(TestCase):
     """Test status code different pages """
+
     # моя ошибка что не использовал reverse!!!
 
     def test_index_page(self):
@@ -67,7 +71,7 @@ class TestStatusCodePages(TestCase):
     def test_filter_item_page(self):
         resp = self.client.get('/filter/item/1/')
         self.assertEqual(resp.status_code, 200)
-        
+
     def test_filter_shop_page(self):
         resp = self.client.get('/filter/shop/1/')
         self.assertEqual(resp.status_code, 200)
@@ -80,8 +84,10 @@ class TestStatusCodePages(TestCase):
         resp = self.client.get('/unsold_items/')
         self.assertEqual(resp.status_code, 200)
 
+
 class TestAmountGoods(TestCase):
     """Test amount goods"""
+
     def setUp(self):
         # create instance, why?
         pass
@@ -100,3 +106,42 @@ class TestAmountGoods(TestCase):
         resp = self.client.get('/index/')
         self.assertContains(resp, 'Amount goods in shops = 0')
         self.assertContains(resp, 'Sorry, but all goods are sold')
+
+
+class TestSelenium(StaticLiveServerTestCase):
+    fixtures = ['initial.json']
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver(executable_path=r'D:\PyTechArt\topic7\Fins\chromedriver.exe')
+        cls.selenium.implicitly_wait(60)
+
+    def tearDown(self):
+        self.selenium.close()
+        super().tearDown()
+
+    def test_login(self):
+        # Login
+        self.selenium.get(self.live_server_url+'/fins/login/')
+        self.selenium.find_element_by_id("id_username").send_keys('anton')
+        self.selenium.find_element_by_id("id_password").send_keys('1111')
+        self.selenium.find_element_by_class_name('btn-success').click()
+
+        # Main index
+        self.selenium.get(self.live_server_url + '/fins/index/')
+        self.selenium.find_element_by_class_name('selections').click()
+        self.selenium.find_element_by_id('option_2').click()
+        self.selenium.find_element_by_class_name('btn_select_shop').click()
+
+        # Detail shop
+        self.selenium.find_element_by_css_selector('input[value="Create new item"]').click()
+        self.selenium.find_element_by_id('id_name').send_keys('Maksim')
+        self.selenium.find_element_by_id('id_description').send_keys('BSTU 3 course 6 poit')
+        self.selenium.find_element_by_id('id_comments').send_keys('Nice guy')
+        self.selenium.find_element_by_id('id_price').send_keys(777.777)
+        self.selenium.find_element_by_css_selector('input[value="Create"]').click()
+        self.selenium.get('%s%s' % (self.live_server_url, '/fins/index/2/'))
+
+
+

a953a5c540ef7780712764f39aa3414e141a6f0f
Anton.TkachouITechArt <Anton.Tkachou@itechart-group.com>
2021-12-14 11:22:13 +0300

    +add models?

--- topic7/test_django/test_project/test_app/models.py
+++ topic7/test_django/test_project/test_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import IntegerField, CharField
+
+
+class TestModeltest(models):
+    name = CharField(max_length=200, null=True, blank=True)
+    number = IntegerField(default=0, null=True, blank=True)
+
+
